c0eb95b97b1e01e55ebb139bacae6f1c0780c962
darshan <darshanmariyanna7@gmail.com>
2026-02-19 10:27:17 +0530

    checking no is even or odd using switch

--- SpecialTwoDigit.java
+++ SpecialTwoDigit.java
@@ -3,14 +3,13 @@ class specialTwoDigit{
     public static void main(String[] args){
         Scanner sc =new Scanner(System.in);
         int n=sc.nextInt();
-        int d1=n/10;
-        int d2=n%10;
-        int sum=d1+d2+d1*d2;
-        if(sum==n){
-            System.out.println("n is special two digit no ");
-        }
-        else{
-            System.out.println("n is not  special two digit no ");
+        switch (n%2) {
+            case 0:
+                System.err.println("even");
+                break;
+            case 1:
+                System.err.println("odd");
+                break;
         }
     }
 }
