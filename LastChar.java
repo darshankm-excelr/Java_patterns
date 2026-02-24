061db4e3cd6109d35e00ea32703c85cc803a3ff5
darshan <darshanmariyanna7@gmail.com>
2026-02-24 20:47:17 +0530

    last character

---
+++ LastChar.java
@@ -0,0 +1,10 @@
+class LastChar{
+    public static void main(String[] args) {
+        String st="LeetCode";
+        if(st.length()>0){
+            char last=st.charAt(st.length()-1);
+            System.out.println(last);
+
+        }
+    }
+}
