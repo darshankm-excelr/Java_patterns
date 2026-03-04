a1e4967a0b6b7c2558d25ffa19ea9ee320d2a2fd
darshan <darshanmariyanna7@gmail.com>
2026-03-04 16:37:59 +0530

    angle b/w hour and minit harm

---
+++ Angle.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+class Angle{
+    
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter the Hour angle:");
+        int hr=sc.nextInt();
+        System.out.println("enter the min angle:");
+        int min=sc.nextInt();
+         double minitAngle=min*6;
+       double  HourAngle= hr*30+min*0.5;
+       double angle=minitAngle-HourAngle;
+       if(angle<0)
+            angle=angle*1;
+        else if(angle>180)
+            angle=360-angle;
+       System.out.println("least angle is"+angle);
+
+    }
+}
