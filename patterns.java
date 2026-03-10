0c6d374c24738e4e3c74e426ceac7d1cc11acdc5
darshan <darshanmariyanna7@gmail.com>
2026-03-10 10:45:43 +0530

    pattern

---
+++ patterns.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+class Pattern{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number:");
+        int n = sc.nextInt();
+
+        for(int i = 1; i <= n; i++){
+            for(int j = 0; j < n; j++){
+                System.out.print(i + j*10 + " ");
+            }
+            System.out.println();
+        }
+    }
+}
