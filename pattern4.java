bba2cefae16a7f17823dc9afefa9b6fe410b8132
darshan <darshanmariyanna7@gmail.com>
2026-03-10 11:09:11 +0530

    pattern5

--- pattern4.java
+++ pattern4.java
@@ -9,7 +9,7 @@ class pattern3{
         int n=sc.nextInt();
         for(int i=1;i<=n;i++){
             for(int j=1;j<=n;j++){
-              System.out.print(i%2+ " ");
+              System.out.print(i+ " ");
             }
             System.out.println();
     }
