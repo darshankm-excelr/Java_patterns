27b5e65114d5dec75997d08e007583a038fd8716
darshan <darshanmariyanna7@gmail.com>
2026-03-15 11:20:16 +0530

    pattern 25

---
+++ pattern25.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class patter25 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter n value: ");
+        int n = sc.nextInt();
+
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= n; j++) {
+              
+
+                if(  j == n || j == 1 ||i==1||i==n||j==n/2+1||i==n/2+1 ) {
+                    System.out.print("* ");
+                }
+                else {
+                    System.out.print("  ");
+                }
+
+            }
+            System.out.println();
+        }
+    }
+}
