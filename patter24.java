c6d970ad61abb887a71d149d44531a4159f82800
darshan <darshanmariyanna7@gmail.com>
2026-03-15 10:57:19 +0530

    pattern 24

---
+++ patter24.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class patter24 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter n value: ");
+        int n = sc.nextInt();
+
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= n; j++) {
+
+                if(i == 1 || i == n || j == 1 || j == n || i == j || i + j == n + 1) {
+                    System.out.print("* ");
+                }
+                else {
+                    System.out.print("  ");
+                }
+
+            }
+            System.out.println();
+        }
+    }
+}
