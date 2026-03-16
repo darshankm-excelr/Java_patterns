6fb7feefc09d4376cf284f6ab611b6f9dff52680
darshan <darshanmariyanna7@gmail.com>
2026-03-16 10:41:58 +0530

    Star patter using star

---
+++ patternStar.java
@@ -0,0 +1,22 @@
+public class patternStar {
+    public static void main(String[] args) {
+
+        int n = 7; 
+
+        for (int i = 0; i < n; i++) {
+            
+            for (int j = 0; j < n; j++) {
+                //i + j == n - 1 || i == n / 2 || j == n / 2
+
+                if (i==j||i+j==n-1 ||i==n/2||j==n/2) {
+                    System.out.print(" * ");
+                } else {
+                    System.out.print("  ");
+                }
+
+            }
+
+            System.out.println();
+        }
+    }
+}
