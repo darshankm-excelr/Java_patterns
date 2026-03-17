861fafa2e7cdb8f13da7a7698c91b88acb39d791
darshan <darshanmariyanna7@gmail.com>
2026-03-17 18:35:31 +0530

    invertedPyramid

---
+++ IntvertedPyramid.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+class InvertedPyramid{
+    public static void main(String []args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter n value:");  
+        int n=sc.nextInt();
+        for(int i=n;i>=1;i--){
+            for (int j=1;j<=n-i;j++){
+                System.out.print(" ");
+            }
+            for(int k=1;k<=i;k++){
+                System.out.print(" *");
+            }
+            System.out.println(); 
+        } 
+    }
+
+}
