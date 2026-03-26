96c740b05b50fce61c5dd12143918a732b8a36a0
darshan <darshanmariyanna7@gmail.com>
2026-03-26 14:17:53 +0530

    reversing first and last element in array

--- Practice.java
+++ Practice.java
@@ -1,27 +1,32 @@
 
 import java.util.Scanner;
 class Practice{
-  public static int SumArray(int a[]){
-int sum=0;int avg=0;
+  public static int[] findMax(int a[]){
     for(int i=0;i<a.length;i++){
-      sum+=a[i];
-      
-    }
-    avg=sum/a.length;
-    return avg;
-    
+    int t=a[0];
+    a[0]=a[a.length-1];
+    a[a.length-1]=t;
+  
+  }
+  return a;
   }
     public static void main(String args[]){
       Scanner sc=new Scanner(System.in);
       System.out.println("Enter the size");
       int n=sc.nextInt();
+      //int key=sc.nextInt();
 
       int a[]=new int[n];
       for(int i=0;i<a.length;i++){
         a[i]=sc.nextInt();
       }
      
-        System.out.println(SumArray(a));
+        int res[]=findMax(a);
+
+        System.out.println("Reveresed array:");
+        for(int i=0;i<res.length;i++){
+            System.out.print(res[i]+" ");
+        }
       
 
 
