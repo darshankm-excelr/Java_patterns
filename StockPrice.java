c9fada04f19a6efb4b927886c0116430b28e3abe
darshan <darshanmariyanna7@gmail.com>
2026-04-02 12:07:00 +0530

    max profit

---
+++ StockPrice.java
@@ -0,0 +1,27 @@
+
+
+class StockPrice{
+    public static int maxProfit(int prices[]){
+        int min=prices[0];
+        int maxProfit=0;
+        for(int i=1;i<prices.length;i++){
+            if(prices[i]<min){
+              min=prices[i];
+            }
+            else {
+                int profit=prices[i]-min;
+                if(profit>maxProfit){
+                    maxProfit=profit;
+
+
+                }
+            }
+           
+        }
+         return maxProfit;
+    }
+    public static void main(String[]args){
+        int x[]={7,10,1,3,6,9};
+        System.out.println(maxProfit(x));
+    }
+}
